cd46417faa0c5e2fa2f07986996bbc3eada399b1
Megatroxx <mobilkaxxx@icloud.com>
2023-05-28 16:10:44 +0400

    AddEventMid2

--- app/src/main/java/com/example/intouch/AddFragment.java
+++ app/src/main/java/com/example/intouch/AddFragment.java
@@ -1,7 +1,13 @@
 package com.example.intouch;
 
+import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.widget.AppCompatButton;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -10,48 +16,28 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 import com.example.intouch.databinding.ActivityMainBinding;
+import com.example.intouch.databinding.FragmentAdd2Binding;
+
 
 
 public class AddFragment extends Fragment {
-
-    String[] item = {"Комфорт и уют", "Спортивный отдых", "Вечеринки", "Культура и развитие", "Другое"};
-
-    AutoCompleteTextView autoCompleteTextView;
-
-    ArrayAdapter<String> adapterItems;
-
-
-    ActivityMainBinding binding;
-
+    FragmentAdd2Binding binding;
+    AppCompatButton WomanButton, ManButton, AllButton;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
 
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        View view = binding.getRoot();
-//
-//        autoCompleteTextView = (AutoCompleteTextView) getView().findViewById(R.id.auto_complete_text);
-//
-//        adapterItems = new ArrayAdapter<String>(getActivity(), R.layout.list_item, item);
-//
-//        autoCompleteTextView.setAdapter(adapterItems);
-//
-//        autoCompleteTextView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-//            @Override
-//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-//                String item = parent.getItemAtPosition(position).toString();
-//                Toast.makeText(getActivity(), "item" + item, Toast.LENGTH_SHORT).show();
-//
-//
-//            }
-//        });
+
+
+
         return inflater.inflate(R.layout.fragment_add2, container, false);
-
-
     }
+
+
 }
