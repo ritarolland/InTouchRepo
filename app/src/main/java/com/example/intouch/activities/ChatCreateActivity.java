d043ccb3053abda1c92e51a450b23e52df65efd9
ritarolland <zakharchenkoveronika@gmail.com>
2023-05-29 12:07:54 +0300

    chat completely merged

--- app/src/main/java/com/example/intouch/activities/ChatCreateActivity.java
+++ app/src/main/java/com/example/intouch/activities/ChatCreateActivity.java
@@ -1,16 +1,101 @@
 package com.example.intouch.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
 
-import com.example.intouch.R;
+import com.example.intouch.TheMostMainActivity;
+import com.example.intouch.adapters.ChatCreateAdapter;
+import com.example.intouch.databinding.ActivityChatCreateBinding;
+import com.example.intouch.listeners.UserListener;
+import com.example.intouch.models.User;
+import com.example.intouch.utils.Constants;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.FirebaseDatabase;
 
-public class ChatCreateActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class ChatCreateActivity extends AppCompatActivity implements UserListener {
+    ActivityChatCreateBinding activityChatCreateBinding;
+    FirebaseDatabase inTouchDataBase = FirebaseDatabase.getInstance();
+    private FirebaseAuth mAuth = FirebaseAuth.getInstance();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_chat_create);
+        activityChatCreateBinding = ActivityChatCreateBinding.inflate(getLayoutInflater());
+        setContentView(activityChatCreateBinding.getRoot());
+        setListeners();
+        setInitialData();
+
+    }
+
+    private void setListeners() {
+        activityChatCreateBinding.buttonBack.setOnClickListener(v -> goBack());
+    }
+
+
+    private void setInitialData() {
+
+        inTouchDataBase.getReference(Constants.KEY_COLLECTION_USERS).get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+
+            @Override
+            public void onComplete(@NonNull Task<DataSnapshot> task) {
+                ArrayList<User> users = new ArrayList<>();
+
+                if (task.isSuccessful() && task.getResult() != null) {
+                    FirebaseUser currentUser = mAuth.getCurrentUser();
+                    String currentUserId = currentUser.getUid();
+
+                    DataSnapshot dataSnapshot = task.getResult();
+
+                    for (DataSnapshot childSnapshot : dataSnapshot.getChildren()) {
+                        if (currentUserId.equals(childSnapshot.getKey())) {
+                            continue;
+                        }
+                        String email = childSnapshot.child(Constants.KEY_USER_EMAIL).getValue().toString();
+
+                        String password = childSnapshot.child(Constants.KEY_USER_PASSWORD).getValue().toString();
+                        String name = childSnapshot.child(Constants.KEY_USER_NAME).getValue().toString();
+                        String id = childSnapshot.getKey();
+
+                        users.add(new User(id, email, password, name));
+
+                    }
+
+
+                }
+                RecyclerView recyclerView = activityChatCreateBinding.users;
+                ChatCreateAdapter chatCreateAdapter = new ChatCreateAdapter(users, ChatCreateActivity.this);
+
+                recyclerView.setAdapter(chatCreateAdapter);
+
+
+            }
+        });
+
+    }
+
+
+    @Override
+    public void onUserClick(User user) {
+        Intent intent = new Intent(getApplicationContext(), ChatActivity.class);
+        intent.putExtra(Constants.KEY_USER, user);
+        startActivity(intent);
+        finish();
+    }
+
+    private void goBack() {
+        Intent intent = new Intent(this, TheMostMainActivity.class);
+        intent.putExtra(Constants.KEY_FRAGMENT_TAG, "Messenger");
+        startActivity(intent);
     }
 }
